0370ea95da077df8ab100123283fa4b03b1d2d1c
Simeon Stamenov <sst@mainformat.de>
2016-01-07 14:14:53 +0100

    fixed bug for not allowing both panels ti be opened at the same time

--- _js/offcanvas.js
+++ _js/offcanvas.js
@@ -2,6 +2,9 @@
     $(document).ready(function() {
         $('[data-toggle="offcanvas"]').click(function(e) {
             e.preventDefault();
+            $('[data-toggle="offcanvas"]').not(this).each(function(i,v){
+            	$($(v).attr("data-target")).removeClass("active");
+            })
             $($(this).attr("data-target")).toggleClass('active');
         });
     });
